9e256b1eb991e93a7cc3a6bc865d7683879b123f
smhood <Hoodster92@gmail.com>
2018-09-10 16:57:40 -0500

    code dev build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './lib/components/Index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
